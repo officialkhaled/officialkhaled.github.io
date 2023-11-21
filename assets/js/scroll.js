792021224e4279e88504f0307d7e11f129cda3d5
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2023-11-21 23:05:33 +0600

    Search Feature Added and Locations Updated

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,3 +1,4 @@
+/* Move X Direction */
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
@@ -12,4 +13,21 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => {
   observer.observe(el);
+});
+
+/* Move Y Direction */
+const observer_1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("move-up");
+    } else {
+      entry.target.classList.remove("move-up");
+    }
+  });
+});
+
+const hiddenElements_1 = document.querySelectorAll(".move-up-hidden");
+hiddenElements_1.forEach((el) => {
+  observer_1.observe(el);
 });
